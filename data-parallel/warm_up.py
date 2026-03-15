4558591784f149cd3769bd0a19536cbf20ac126a
Samet <samet.balkan@chaptr.com>
2026-03-15 16:45:38 +0100

    torch distributed warm-up 2

--- data-parallel/warm_up.py
+++ data-parallel/warm_up.py
@@ -1,6 +1,7 @@
 import os
 
 import torch
+from torch import distributed
 
 
 def setup():
@@ -10,7 +11,27 @@ def setup():
 
     device_count = torch.cuda.device_count()
 
+    device = torch.device(f"cuda:{local_rank}")
+    torch.cuda.set_device(device)
+
     print(f"rank: {rank} | local rank: {local_rank} | world_size: {world_size} | device count: {device_count}")
+
+    distributed.init_process_group(backend="nccl", rank=rank, world_size=world_size, device_id=device)
+
+    t = torch.tensor([rank], device=device)
+
+    s = torch.arange(8, device=device)
+
+    gather_slots = [torch.zeros(1, device=device) for _ in range(world_size)]
+    distributed.all_gather(gather_slots, t)
+
+    print(f"Device {rank} all-gather: {torch.concat(gather_slots)}")
+
+    scatter_output = torch.zeros(2, device=device)
+
+    distributed.reduce_scatter_tensor(scatter_output, s)
+
+    print(f"Device {rank} reduce-scatter: {scatter_output}")
 
 if __name__ == "__main__":
     setup()
